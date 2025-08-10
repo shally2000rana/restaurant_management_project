3662ce856b6d66a9fae37c3ffe4b7652cdb848ff
shally2000rana <itsmeshallyrana2000@gmail.com>
2025-08-10 15:53:09 +0530

    26556

--- manage.py
+++ manage.py
@@ -1,8 +1,49 @@
-RESTAURANT NAME="The great indian kitchen"
-from django.conf import settingd
-from django.shortcuts import render
+<!DOCTYPE html>
+<html>
+<head>
+  <title>A bout us- My Restaurant</title>
+  <style>
+    body{
+        font-family:Arial,sans-serif;
+        background-color:#f7f7f7;
+        margin:0;
+        padding:0;
+    }
+    header{
+        background-color:#333;
+        color:white;
+        margin:0;
+        padding:0;
+    }
+    main{
+        max-width:80px;
+        margin:auto;
+        background:white;
+        padding:2em;
+        box-shadow:0px 2px 8px rgba(0,0,0,.1);
+    }
+    img{
+        width:100px;
+        border-radius:8px;
+        margin-bottom: 1em;
+    }
+    h1{
+        text-align:center;
+    }
+  </style>
+  <header>
+  <h2> My Restaurant</h2>
+  <main>
+    <img src="{% static 'images/testaurant.jpg' %}" alt="Restaurant Image">
+    <h1>About us</h1>
+    <p>welcome to my Restaurant, where we serve delicious and freshly prepared meals with love.
+      our mission is to create a cozy atmosphere where friends and families can enjoy unforgetable dining experiences.</p>
+    <p>we pride ourselves on using the freshest ingredient ,sourced locally whenever possible,to bring you flavours that make every bite special.</p>
+ </main>
+ </body>
+ </html>
 
-def home(request):
-    return render(request,'home.html',{'restaurant_name': settings.RESTAURANT_NAME})
-    
- <h1>Welcome to{{restaurant_name}}</h1>   
+<nav>
+  <a href="{% url 'home' %}">Home</a>
+  <a href="{% url'about' %}">About</a>
+</nav>  

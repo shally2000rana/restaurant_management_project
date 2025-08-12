f312ffb81bb9b2581fec7c508656b76cfb10f70d
shally2000rana <itsmeshallyrana2000@gmail.com>
2025-08-12 21:53:16 +0530

    28925

--- manage.py
+++ manage.py
@@ -1,78 +1,67 @@
-from django.shortcuts import render
-def menu_list (request):
-    menu_items=[
-        {'name':'Margherita Pizza', 'price':299},
-        {'name':'Veg Burger', 'price':149},
-        {'name':'Pasta Alfredo','price':249}
-    ]
-    context={
-        'menu_items':menu_items
-    }
-    return render(request,'menu_list.html',context)
-    <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
  <meta charset="UTF-8">
  <Meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Menu</title>
+  <title>{{restaurant name}}</title>
   <style>
     body{
-        font-family:Arial,sans-serif;
+        font-family:'Segoe UI',Tahoma, Geneva, Verdana,sans-serif;
         background-color:#f8f9fa;
         margin:0;
         padding:0;
     }
-    .container{
-        max-width:600px;
-        margin:40px auto;
-        background:#fff;
-        padding:20px;
-        border-radius:8px;
-        box-shadow:0 4px 8px rgba(0,0,0,0.1);
+    header{
+        background-color:#ff5733;
+        text-align:center;
+        padding:20px 0;
+        color: white;
+        font-size:28px;
+        letter-spacing:1px;
         
     }
-    h2{
-        text-align:center;
-        color:#333;
-    }
-    ul{
-        list-style:none;
-        padding:0;    
-    }
-    li{
-        padding:10px;
-        border-bottom:1px solid #ddd;
-        display:flex;
-        justify-content:space-between;
-    }
-    li:last-child{
-        border-bottom:none;
+    .container{
+       max-width:800px;
+       margin:50px auto;
+       background:white;
+       padding: 30px;
+       border-radius:8px;
+       box-shadow:0px 4px 10px rgba(0,0,0,0.1);
+       text-align:center;
 
     }
-    .price{
-        color:#555;
+    h1{
+       color:#333;
+       font-size:36px;
+       margin-nottom:10px;    
     }
+    p{
+        color:#666;
+        font-size:18px;
+        line-height:1.6;
+    }
+    footer{
+        text-align:center;
+        background-color:#ff5733;
+        color:white;
+        padding:15px 0;
+        margin-top:50px;
+
+    }
+    
   </style>
    
 </head>
 <body>
  <div class="container">
-  <h2>Our Menu</h2>
-  <ul>
-    {% for item in menu_items %}
-      <li>
-        <span>{{item.name}}</span>
-        <span class="price">{{item.price}}</span>
-      </li>
-    </ul>  
+  <h1>welcome to {{restaurant_name}}!</h1>
+  <p>we are happy to serve you the best food in town.
+  Enjoy our delicious menu and cozy atmosphere</p>
  </div>
+ <footer>
+    &copy; 2025{{restaurant_name}}.All rights reserved.
+ </footer>
 </body>
 </html>
-from django.urls import path
-from . import views
-
-urlpatterns=[
-    path('menu/', views.menu_list, name='menu-list'),
-]
 
  

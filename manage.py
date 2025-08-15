b5b3aca8428db766caaf1d6e52a904a23d1b68cb
shally2000rana <itsmeshallyrana2000@gmail.com>
2025-08-15 10:49:15 +0530

    31930

--- manage.py
+++ manage.py
@@ -1,15 +1,23 @@
-from django.shortcuts import render
-from djanfo.http import HttpResponse
-from .models import Product
-from django.db import DatabaseError
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Restaurant Homepage</title>
+   <style>
+     .logo{
+        display:block;
+        margin:20px auto;
+        max-width:200px;
+        height:auto;
+     }
+   </style>
+</head>
+<body>
+    <header>
+        <img src="{% static 'images/restaurant_logo.png' %}" alt="Restaurant Logo" class="logo">
+    </header>
+    <h1>welcome to our restaurant </h1>
+</body>
+</html>
 
-def product_list(request):
-    try:
-        products=Product.objects.all()
-        return render(request,'product_list.html',{'products':products})
-    except DatabaseError:
-        return HttpResponse("sorry, there was a problem fetching the products. Please try again later.",status=500)
-    except Exception as e:
-        retrun HttpResponse(f"An unexpected error occured: {str(e)}", status=500)
 
  

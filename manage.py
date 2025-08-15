26ac94a84aae1031a4a9d3dad667827b2db35396
shally2000rana <itsmeshallyrana2000@gmail.com>
2025-08-15 10:42:12 +0530

    31931

--- manage.py
+++ manage.py
@@ -1,47 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
- <meta charset="UTF-8">
- <Meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>HomePage</title>
-  <style>
-    body{
-        font-family:Arial,sans-serif;
-        background-color:#f4f4f4:
-        color:#333;
-        margin:0;
-        padding:20px;
-        line-height:1.6;
-    }
-    .container{
-       max-width:960px;
-       margin:auto;
-       background:#fff;
-       padding:20px;
-       border-radius:8px;
-       box-shadow: 0 0 10px rgba(0,0,0,0.1);
-    }
-    h1{
-       color:#007BFF;
-       text-align:center;
-       margin-bottom:20px;    
-    }
-    p{
-        font-size:1.1rem;
-        text-align:center;
-    }
- 
- </style>
-   
-</head>
-<body>
-  
+from django.shortcuts import render
+from djanfo.http import HttpResponse
+from .models import Product
+from django.db import DatabaseError
 
- <div class="container">
-  <h1>weicome to our website</h1>
-  <p>this is the homepage template.it's a great starting point for your project.</p>
- </div>
-</body>
-</html>
+def product_list(request):
+    try:
+        products=Product.objects.all()
+        return render(request,'product_list.html',{'products':products})
+    except DatabaseError:
+        return HttpResponse("sorry, there was a problem fetching the products. Please try again later.",status=500)
+    except Exception as e:
+        retrun HttpResponse(f"An unexpected error occured: {str(e)}", status=500)
 
  

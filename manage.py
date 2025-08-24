06fb86a7f5c0dbdecd0a5d994f0db8313bc34192
shally2000rana <itsmeshallyrana2000@gmail.com>
2025-08-24 12:37:49 +0530

    37478

--- manage.py
+++ manage.py
@@ -3,54 +3,46 @@
 <head>
    <title>Restaurant Homepage</title>
    <style>
-     .logo{
-        display:block;
-        margin:20px auto;
-        max-width:200px;
-        height:auto;
-     }
+    
      body{
         font-family:Arial, sans-serif;
-        padding: 20px;
+        padding: 0;
+        margin:0;
+        min-height:100vh;
+        display:flex;
+        fex-direction:column;
+     }
+     main{
+        flex: 1;
+        padding:20px;
+     }
+    
+  
+     footer{
+        padding:15px 0;
+        background-color:#333;
+        color:#fff;
         text-align:center;
-     }
-     .search-container{
-        margin: 20px auto;
-        max-width:400px;
-     }
-     input[type="text"]{
-        width:70%;
-        padding:10px;
-        border-radius:8px;
-        border:1px solid #ccc;
-        font-size:16px;
-     }
-     button{
-        padding:10px 15px;
-        border-radius:8px;
-        border:none;
-        background-color:#28a745;
-        color:white;
-        font-size:16px;
-        cursor:pointer;
-     }
-     button:hover{
-        background-color:#218838;
+        margin-top:auto;
      }
    </style>
 </head>
 <body>
-    <header>
-        <img src="{% static 'images/restaurant_logo.png' %}" alt="Restaurant Logo" class="logo">
-    </header>
+    <main>
+       {% block title %} Home- Restaurant{%end block %}
+       {% block content %}
+    </main>
     <h1>welcome to our restaurant </h1>
-
+    <p>Delicious food, made with love.</p>
+   <footer>
     <div class="search-container">
       <form method="GET" action="">
          <input type="text" name="q" placeholder="Search menu items...">
          <button type="submit">Search</button>
       </form>
     </div>
+    </footer>
+  {% endblock %}
 </body>
 </html>
 

98770108a23212cb3a28f6f1269128b093b525d6
shally2000rana <itsmeshallyrana2000@gmail.com>
2025-08-26 19:31:03 +0530

    38476

--- manage.py
+++ manage.py
@@ -1,31 +1,20 @@
-from django.db import models
-from django.contrib.auth.models import User
+from django.contrib import admin
+from .models import Menu, Order, OrderItem
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display=('id', 'name','price')
+    search_fields=('name')
+    list_filter=('price')
 
-class Order(models.Model):
-    STATUS_CHOICES=[
-        ('PENDING','Pending'),
-        ('CONFIRMED','Confirmed'),
-        ('DELIVERED','Delivered'),
-        ('CANCELLED','Cancelled'),
-    ]
+class OrderItemInLine(admin.TabularInLine):
+    model=OrderItem
+    extra=1
 
-    customer=models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
-    total_amount=models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    status=models.CharField(max_length=10, choices=STATUS_CHOICES, default='PENDING')
-    created_at=models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"Order #{self.id} by {self.customer.username}"
-
-class OrderItem(models.Model):
-    order=models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
-    menu_item=models.ForeignKey("Menu", on_delete=models.CASCADE)
-    quantity=models.PositiveIntegerField(default=1)
-
-    def __str__(self):
-        return f"{self.quantity} x {self.menu_item.name}"
-    
-    def get_total_price(self):
-        retrun self.menu_item.price *self.quantity
+@amin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display=('id', 'customer', 'status', 'total_amount', 'created_at')
+    list_filter=('status', 'created_at')
+    search_fields=('customer__username')
+    inlines=[OrderItemInLine]    
 
  

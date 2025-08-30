f0fc1e0e727cd1d84c99c3d41005f43ab4d42a7f
shally2000rana <itsmeshallyrana2000@gmail.com>
2025-08-30 12:14:10 +0530

    40528

--- manage.py
+++ manage.py
@@ -1,26 +1,17 @@
-#settings.py
-RESTAURANT_NAME="Tasty Bites"
-#views.py
-from django.conf import settings
-from django.shortcuts import render
-from django.urls import path
-from . import views
+#models.py
+from django.db import models
+from django.contrib import admin
+from .models import MenuItem
 
-def home(request):
-    restaurant_name=settings.RESTAURANT_NAME
-    return render(request,"home.html", {"rrestaurant_name": restaurant_name})
+class MenuItem(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.TextField()
+    price=models.DecimalField(max_digits=6, decimal_places=2)
 
-urlpatterns=[
-    path('', views.home, name='home').
-]
+    def __str__(self):
+        return self.name
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>{{restaurant_name}}</title>
-</head>
-<body>
-    <h1>Welcome to {{restaurant_name}}</h1>
-</body>
-</html>
- 
+python manage.py makemigrations
+python manage.py migrate
+
+admin.site.register(MenuItem)

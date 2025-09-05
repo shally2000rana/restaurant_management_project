7780820424a7742f6c9c12f4e5487aaa1fb153b9
shally2000rana <itsmeshallyrana2000@gmail.com>
2025-09-05 09:25:51 +0530

    42581

--- manage.py
+++ manage.py
@@ -15,19 +15,11 @@ MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
-from .import views
+from django.urls import path,include
 
 urlpatterns=[
-    path('menu/',view.menu_view, name='menu'),
+    path('',include('your_app.urls')),
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-from django.shortcuts import render
-from .models import MenuItem
-
-def menu_view(request):
-    items=MenuItem.objects.all()
-    return render(request, 'menu.html',{'items':items})
 

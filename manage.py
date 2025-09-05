ef9c617ecbc26e6fcb0b614773c8c5a20e712075
shally2000rana <itsmeshallyrana2000@gmail.com>
2025-09-05 08:58:38 +0530

    42583

--- manage.py
+++ manage.py
@@ -1,13 +1,31 @@
 from django.db import models
 
-class RestaurantLocation(models.Model):
-    address=models.CharField(max_length=255)
-    city=models.CharField(max_length=100)
-    state=models.CharField(max_length=100)
-    zip_code=models.CharField(max_length20)
+class ContactSubmission(models.Model):
+    name=models.CharField(max_length=100)
+    email=models.EmailField()
+    submitted_at=models.DateTimeField(auto_now+add=True)
 
     def __str__(self):
-        return f"{self.address},{self.city},{self.state} _{self.zip_code}"
+        return f"{self.name} ({self.email})"
 
+from django import forms
+from .models import ContactSubmission
 
-  
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model=ContactSubmission
+        fields=['name','email']
+
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+
+def contact_view(request):
+    if request.method=='POST':
+        form=ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('thank_you')
+        else:
+            form=ContactForm()
+        return render(request, 'contact.html',{'form':form})
+

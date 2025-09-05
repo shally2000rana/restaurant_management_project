791cb5182cbb0e9dec8b5e1b45a37f86ad9c22c5
shally2000rana <itsmeshallyrana2000@gmail.com>
2025-09-05 08:45:18 +0530

    42584

--- manage.py
+++ manage.py
@@ -1,37 +1,13 @@
 from django.db import models
-class Restaurant(models.Model):
-    name=models.CharField(max_length=100)
-    address=models.TextField()
+
+class RestaurantLocation(models.Model):
+    address=models.CharField(max_length=255)
+    city=models.CharField(max_length=100)
+    state=models.CharField(max_length=100)
+    zip_code=models.CharField(max_length20)
 
     def __str__(self):
-        return self.name
-from django.contrib import admin
-from .models import Restaurant
-admin.site.register(Restaurant)
-python manage.py makemigrations
-python manage.py migrate
-
-from django.shortcuts import render
-from .models import Restaurant
-
-def homepage(request):
-    restaurant =Restaurant.objects.first()
-    return render(request,"homepage.html",{"restaurant":restaurant})
-from django.urls import path
-from . import pathurlpatterns=[
-    path("",views.homepage,name="homepage"),
-
-]
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Restaurant Homepage</title>
-</head>
-<body>
-    <h1>Welcome to {{restaurant.name}}</h1>
-    <p><strong>Address:</strong>{{restaurant.address}}</p>
-</body>
-</html>
+        return f"{self.address},{self.city},{self.state} _{self.zip_code}"
 
 
   

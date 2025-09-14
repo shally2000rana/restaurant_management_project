0f6e3b447a5b5beae61ecf15a5885ea823426455
shally2000rana <itsmeshallyrana2000@gmail.com>
2025-09-14 09:31:05 +0530

    44283

--- manage.py
+++ manage.py
@@ -1,33 +1,11 @@
+RESTAURANT_PHONE="+91 98765 43210"
+from django.conf import settings
+from django.shortcuts import render
 
+def home(request):
+    return render(request, 'home.html', {'phone':settings.RESTAURANT_PHONE})
 
-{% extends 'base.html' %}
-{% block title %}Home{% endblock %}
-{% block breadcrumbs %}
-<div class="breadcrumb">
-  <span>Home</span>
+<div class="contact-info">
+   <strong>Call Us:</strong>{{phone}}
 </div>
-{% endblock %}
-
-{% block content %}
-   <div class="welcome-section">
-     <h1>welcome to our Restaurant</h1>
-     <p class="tagline">"where flavour meets tradition -serving happiness on every plate."</p>
-   </div>
-   <p>explore our menu, history, and more</p>
-{% endblock %}
-
-.welcome-section{
-    text-align:center;
-    margin:40px 0;
-}
-.welcome-section h1{
-    font-size:2.5rem;
-    color:#333;
-    margin-bottom:10px;
-}
-.welcome-section .tagline{
-    font-size:1.2rem;
-    color:#666;
-    font-style: italic;
-}
 

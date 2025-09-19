36b5175d027886fa5a56e23cdeedf0151376799c
shally2000rana <itsmeshallyrana2000@gmail.com>
2025-09-19 10:38:05 +0530

    47319

--- manage.py
+++ manage.py
@@ -1,26 +1,13 @@
-import string
-import secrets
-from .models import Coupon
+pip install djangorestframework
 
-def generate_coupon_code(length=10):
-    """
-    Generate a unique alphanumeric coupon code
-    """
-    characters=string.ascii_uppercase+string.digits
+INSTALLED_APPS=[
 
-    while True:
-        code=''.join(secrets.choice(characters)for _in range(length))
-        if not Coupon.objects.filter(code=code).exists():
-            return code
-
-from django.db import models
-class Coupon(models.Model):
-    code=models.CharField(max_length=20, unique=True)
-    discount=models.DecimalFiels(max_length=5, decimal_places=2)
-    active=models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.code
-from orders.utils import generate_coupon_code
-
-new_code=generate_coupon_code(12)
+    'rest_framework',
+    'home',
+  ]
+from rest_framework import serializers
+from .models import MenuCategory
+class MenuCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model=MenuCategory
+        fields=['id','name']

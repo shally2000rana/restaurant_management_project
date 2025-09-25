8edf3b5826062f8a603f4bc22be6c368c00dda3a
shally2000rana <itsmeshallyrana2000@gmail.com>
2025-09-25 12:22:11 +0530

    49222

--- manage.py
+++ manage.py
@@ -1,16 +1,20 @@
-from django.core.validators import validate_email
-from django.core.exceptions import ValidationError
+from rest_framework.views import APIView
+from rest_framework.response import response
+from rest_framework import status
+from .models import MenuItems
+from .serializers import MenuItemsSerializer
 
-def is_valid_email(email: str) ->bool:
-    """
-    Validate an email address using Django's built-in email validator.
-    Returns True if valid, False otherwise.
-    """
-    try:
-        validate_email(email):
-        return True:
-    except ValidationError:
-        return False
-    except Exceptions as e:
-        print(f"Unexpected error during email validation :{e}")
-        return False
+class MenuItemByCategory(APIView):
+    def get(self, request, *args, **kwargs):
+        """
+        An API endpointto retreive menu items
+        filtered by category.
+        """
+        category_name=request.query_params.get('category_name') 
+        if not category_name:
+            return Response({"error":"Category name parameter is required."}status=status.HTTP_400_BAD_REQUEST)
+
+        menu_items=MenuItems.objects.filter(category_name=category_name)
+
+    MenuItemsSerializer(menu_items, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)

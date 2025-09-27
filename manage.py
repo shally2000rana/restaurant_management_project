8e93c9845a99273e4c3b422005be6ace2102f9a0
shally2000rana <itsmeshallyrana2000@gmail.com>
2025-09-27 08:31:33 +0530

    51270

--- manage.py
+++ manage.py
@@ -1,27 +1,9 @@
-from rest_framework import serializers
-from django.contrib.auth.models import User
-
-class UserProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=User
-        fields=["first_name","last_name","email"]
-
-from rest_framework.generics import UpdateAPIView
-from rest_framework.permissions import IsAuthenticated
-from django.contrib.auth.models import User
-from .serializers import UserProfileSerializer
-
-class UserProfileUpdateView(UpdateAPIView):
-    serializer_class=UserProfileSerializer
-    permission_classes=[IsAuthenticated]
-
-    def get_object(self):
-        return self.request.User
-
-from django.urls import path
-from .views import UserProfileUpdateView
-
-urlpatterns=[
-    path("profile/update/", UserProfileUpdateView,as_view(), name="profile-update"),
-]
+#in your models.py file(assuming a Django-like structure)
+class Restaurant(models.Model):
+    operaating_days=models.CharField(
+        max_length=50,
+        blank=True,
+        default=",
+        help_text="Comma-separated list of operating days"
+    )
 

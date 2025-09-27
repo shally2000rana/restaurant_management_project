53799517daaf8a78f4c8730a9c14818a0d147379
shally2000rana <itsmeshallyrana2000@gmail.com>
2025-09-27 08:13:59 +0530

    51268

--- manage.py
+++ manage.py
@@ -1,39 +1,37 @@
-from django.db import models
-from django.contrib.auth.models import User
-from home.models import Product
-
-class Order(models.Model):
-    customer=models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
-    order_items=models.ManyToManyField(Product, related_name="orders")
-    total_price=models.DecimalField(max_digits=10, decimal_places=2)
-    created_at=models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"Order {self.id} by {self.customer.username}"
-
-from rest_framework import serializers
-from .models import Order
-from home.models import Product
-
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Product
-        fields=["id","name","price"]
-
-class OrderSerializer(serializers.ModelSerializer):
-    order_items=ProductSerializer(many=True)
-    customer=serializers.StringRelatedField()
-
-    class Meta:
-        model=Order
-        fields=["id","customer","order_items","total_price","created_at"]
-
-from rest_framework.generics import RetrieveAPIView
-from .models import Order
-from .serializers import OrderSerializer
-
-class OrderDetailView(RetrieveAPIView):
-    query_set=Order.objects.all()
-    serializer_class=OrderSerializer
-    lookup_field="id"
+EMAIL_BACKEND="django.core.mail.backend.smtp.EmailBackend"
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
+EMAIL_HOST_USER="your_email@gmail.com"
+EMAIL_HOST_PASSWORD="your_app_password"
+DEAULT_FROM_EMAIL=EMAIL_HOST_USER
+from django.core.mail import send_mail, BadHeaderError
+from django.conf import settings
+import logging
+logger=logging.getLogger(__name__)
+def send_order_confirmation_email(order_id, customer_email, order_items, total_price):
+    """
+    Sends an order confirmation email to the customer.
+    """
+    subject=f"Order confirmation - order #{order_id}"
+    message=(
+        f"Thank you for your order!\n\n"
+        f"order ID: {order_id}\n"
+        f"Items: {','join(order_items)}\n"
+        f"Total Price: {total_price}\n\n"
+        f"We will notify you once your order is shipped.\n\n"
+        f"Best Regards, \nSmart Restaurant Team"
+    )
+    from_email=settings.DEAULT_FROM_EMAIL
+    receipent_list=[customer_email]
+    try:
+        send_mail(subject, message, from_email, receipent_list, fail_silently=False)
+        logger.info(f"order confirmation email sent to {customer_email} for order ID {order_id}")
+        return True
+    except BadHeaderError:
+        logger.error("Invalid header found while sending email")
+        return False
+    except Exception as e:
+        logger.error(f"Error sending email : {e}")
+        return False
 

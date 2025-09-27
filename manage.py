09f3e493a0e1079574d42baba0459f8a3b577419
shally2000rana <itsmeshallyrana2000@gmail.com>
2025-09-27 08:23:49 +0530

    51269

--- manage.py
+++ manage.py
@@ -1,37 +1,27 @@
-EMAIL_BACKEND="django.core.mail.backend.smtp.EmailBackend"
-EMAIL_HOST="smtp.gmail.com"
-EMAIL_PORT=587
-EMAIL_USE_TLS=True
-EMAIL_HOST_USER="your_email@gmail.com"
-EMAIL_HOST_PASSWORD="your_app_password"
-DEAULT_FROM_EMAIL=EMAIL_HOST_USER
-from django.core.mail import send_mail, BadHeaderError
-from django.conf import settings
-import logging
-logger=logging.getLogger(__name__)
-def send_order_confirmation_email(order_id, customer_email, order_items, total_price):
-    """
-    Sends an order confirmation email to the customer.
-    """
-    subject=f"Order confirmation - order #{order_id}"
-    message=(
-        f"Thank you for your order!\n\n"
-        f"order ID: {order_id}\n"
-        f"Items: {','join(order_items)}\n"
-        f"Total Price: {total_price}\n\n"
-        f"We will notify you once your order is shipped.\n\n"
-        f"Best Regards, \nSmart Restaurant Team"
-    )
-    from_email=settings.DEAULT_FROM_EMAIL
-    receipent_list=[customer_email]
-    try:
-        send_mail(subject, message, from_email, receipent_list, fail_silently=False)
-        logger.info(f"order confirmation email sent to {customer_email} for order ID {order_id}")
-        return True
-    except BadHeaderError:
-        logger.error("Invalid header found while sending email")
-        return False
-    except Exception as e:
-        logger.error(f"Error sending email : {e}")
-        return False
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=User
+        fields=["first_name","last_name","email"]
+
+from rest_framework.generics import UpdateAPIView
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
+from .serializers import UserProfileSerializer
+
+class UserProfileUpdateView(UpdateAPIView):
+    serializer_class=UserProfileSerializer
+    permission_classes=[IsAuthenticated]
+
+    def get_object(self):
+        return self.request.User
+
+from django.urls import path
+from .views import UserProfileUpdateView
+
+urlpatterns=[
+    path("profile/update/", UserProfileUpdateView,as_view(), name="profile-update"),
+]
 

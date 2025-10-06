b0027808a7eb0c36fa314584938ad5682a0fe6d9
shally2000rana <itsmeshallyrana2000@gmail.com>
2025-10-06 10:16:45 +0530

    53263

--- manage.py
+++ manage.py
@@ -1,20 +1,12 @@
 #orders/utils.py
-from datetime import datetime
-from django.db.models import Sum
-from .models import orders
-def get_daily_sales_total(date_obj.date)-> float:
-    """
-    Calculates the total sales for a specific date by summing up
-    the total price of all orders placed on that day.
-    Args: 
-      date_obj:A date object from Python's
-      datetime modele (e.g., datetime.date.today()).
-      Returns:
-        The calculated total sales (a float or Decimal )for the day.
-        Returns 0 if no orders are found.
-    """
-    orders_on_date=
-    Orders.objects.filter(created_a6t_date=date_obj)
-    total_sales=orders_on_date.aggragate(total_sum=Sum('total_price'))
-    return total_sales.get('total_sum') or 0.0
-     
+from datetime import datetime, time
+def is_resyautant_open():
+    now==datetime.now()
+    current_time=now.time()
+    current_day=now.weekday()
+    opening_time=time(0,0)
+    closing_time=time(22,0)
+    if opening_time <= current_time <=closing_time:
+        return True
+    return False
+         

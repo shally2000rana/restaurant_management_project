1fd08d7b6162eb801600e61b22f42eefbab64361
shally2000rana <itsmeshallyrana2000@gmail.com>
2025-10-06 10:26:06 +0530

    53264

--- manage.py
+++ manage.py
@@ -1,12 +1,7 @@
-#orders/utils.py
-from datetime import datetime, time
-def is_resyautant_open():
-    now==datetime.now()
-    current_time=now.time()
-    current_day=now.weekday()
-    opening_time=time(0,0)
-    closing_time=time(22,0)
-    if opening_time <= current_time <=closing_time:
-        return True
-    return False
+from rest_framework import generics
+from .models import Table
+from .serializers import TableSerializer
+class TableDetailView(generics.RetrieveAPIView):
+    querset=Table.objects.all()
+    serializer_class=TableSerializer
          

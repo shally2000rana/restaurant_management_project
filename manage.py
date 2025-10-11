d9d1740381070220bd9ebe10541181f0b98be0e9
shally2000rana <itsmeshallyrana2000@gmail.com>
2025-10-11 10:05:09 +0530

    54469

--- manage.py
+++ manage.py
@@ -1,15 +1,3 @@
-from django.db import models
-class Order(models.Model):
-    STATUS_CHOICES=[
-        ('Pending','Pending'),
-        ('Processing','Processing'),
-        ('Shipped','Shipped'),
-        ('Delivered','Delivered'),
-        ('Canceled','Canceled'),
-    ]
-    status=models.CharField(
-        max_length=20,
-        choices=STATUS_CHOICES,
-        default='Pending'
-    )
+from rest_framework import serializers
+from .models import Order
          

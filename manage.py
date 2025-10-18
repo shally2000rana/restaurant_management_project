e28d294124befbc399d0bc6cf3e5191dc33fd6c6
shally2000rana <itsmeshallyrana2000@gmail.com>
2025-10-18 09:55:57 +0530

    55546

--- manage.py
+++ manage.py
@@ -1,3 +1,26 @@
+from django.db import models
+class MenuCategory(models.Model):
+    name=models.CharField(max_length=100, unique=True)
+    def __str__(self):
+        return self.name
 from rest_framework import serializers
-from .models import Order
+from .models import MenuCategory
+class MenuCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model=MenuCategory
+        fields='__all__'
+from rest_framework import viewsets
+from .models import MenuCategory
+from .serializers import MenuCategorySerializer
+class MenuCategorySerializer(viewsets.ModelViewSet):
+    queryset=MenuCategory.objects.all()
+    serializer_class=MenuCategorySerializer
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import MenuCategoryViewSet
+router=DefaultRouter()
+router.register(r'menu-categories', MenuCategoryViewSet)
+urlpatterns=[
+    path('', include(router.urls))
+]
          

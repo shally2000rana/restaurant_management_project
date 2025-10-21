d4806cf663b9e1da739673848aa30222c509b022
shally2000rana <itsmeshallyrana2000@gmail.com>
2025-10-21 10:36:30 +0530

    55545

--- manage.py
+++ manage.py
@@ -1,14 +1,5 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import response
-from rest_framework import status
-@api_view(['GET'])
-def get_order_status(reuest, order_id):
-    """
-    Retrieve the current status of an order given its ID.
-    """
-    try:
-        order=Order.objects.get(id=order_id)
-    except Order.DoesNotExist:
-        return response
+from django.db import models
+from django.contrib.auth import get_user_model
+User=get_user_model() 
         
          

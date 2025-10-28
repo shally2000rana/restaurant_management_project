c0006a1ecc436afccbb969d2b3c2999b3a78bfb5
shally2000rana <itsmeshallyrana2000@gmail.com>
2025-10-28 18:24:53 +0530

    56508

--- manage.py
+++ manage.py
@@ -1,55 +1,23 @@
-
 from django.db import models
-
-class Ingredient(models.Model):
-    name=models.CharField(max_length=100)
-    
-    def __str__(self):
-        return self.name
+from django.db.models import Count
+class MenuItemManager(models.Manager):
+    def get_top_selling_items(self, num_items=5):
+        return self.annotate(order_count=Count('orderitem'))\
+                   .order_by('order_count')[:num_items]
 
 class MenuItem(models.Model):
     name=models.CharField(max_length=100)
-    ingredients=models.ManyToManyField(Ingredient, related_name='menu_items')
-
+    price=models.DecimalField(max_digits=6, decimal_places=2)
+    objects=MenuItemManager()
     def __str__(self):
         return self.name
 
-from rest_framework import serializers
-from .models import MenuItem, Ingredient
+class OrderItem(models.Model):
+    order=models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    menu_item=models.ForeignKey(MenuItem, on_delete=models.CASCADE, related_name='orderitem')
+    quantity=models.PositiveIntegerField(default=1)
 
-class IngredientSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Ingredient
-        fields=['id','name']
-
-class MenuItemSerializer(serializers.ModelSerializer):
-    ingredients=IngredientSerializer(many=True, read_only=True)
-
-    class Meta:
-        model=MenuItem
-        fields=['id','name','ingredients']
-
-from rest_framework.views import APIView
-from rest_framework.response import response
-from rest_framework import status
-from .models import MenuItem
-from .serializers import IngredientSerializer
-
-class MenuItemSerializer(APIView):
-    def get(self, request, pk):
-        try:
-            menu_items=MenuItem.objects.get(pk=pk)
-        except MenuItem.DoesNotExist:
-            return Response({'error': 'Menu item not found'}, status=status.HTTP_404_NOT_FOUND)
-
-        ingredients=menu_items.ingredients.all()
-        serializer= IngredientSerializer(ingredients, many=True)
-        return Response(serializer.data)
-
-from django.urls import path
-from .views import MenuItemIngredientsView
-
-urlpatterns=[
-    path('api/menu-items/<int:pk>/ingredients/', MenuItemIngredientsView.as_view(), name='menuitem-ingredients'),
-]
+    def __str__(self):
+        return f"{self.menu_item.name} (x{self.quantity})"
+        
          

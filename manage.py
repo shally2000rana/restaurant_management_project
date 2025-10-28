a1dc34bd51d6946844eea4c54e46503461784978
shally2000rana <itsmeshallyrana2000@gmail.com>
2025-10-28 18:04:07 +0530

    56507

--- manage.py
+++ manage.py
@@ -1,19 +1,55 @@
-import random
+
 from django.db import models
 
-class DailySpecial(models.Model):
+class Ingredient(models.Model):
     name=models.CharField(max_length=100)
-    description=models.TextField()
-    price=models.DecimalField(max_digits=6, decimal_places=2)
-
-    @staticmethod
-    def get_random_special():
-        specials=list(DailySpecial.objects.all())
-        if not specials:
-            return None
-        return random.choice(specials)
     
     def __str__(self):
         return self.name
-        
+
+class MenuItem(models.Model):
+    name=models.CharField(max_length=100)
+    ingredients=models.ManyToManyField(Ingredient, related_name='menu_items')
+
+    def __str__(self):
+        return self.name
+
+from rest_framework import serializers
+from .models import MenuItem, Ingredient
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Ingredient
+        fields=['id','name']
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    ingredients=IngredientSerializer(many=True, read_only=True)
+
+    class Meta:
+        model=MenuItem
+        fields=['id','name','ingredients']
+
+from rest_framework.views import APIView
+from rest_framework.response import response
+from rest_framework import status
+from .models import MenuItem
+from .serializers import IngredientSerializer
+
+class MenuItemSerializer(APIView):
+    def get(self, request, pk):
+        try:
+            menu_items=MenuItem.objects.get(pk=pk)
+        except MenuItem.DoesNotExist:
+            return Response({'error': 'Menu item not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        ingredients=menu_items.ingredients.all()
+        serializer= IngredientSerializer(ingredients, many=True)
+        return Response(serializer.data)
+
+from django.urls import path
+from .views import MenuItemIngredientsView
+
+urlpatterns=[
+    path('api/menu-items/<int:pk>/ingredients/', MenuItemIngredientsView.as_view(), name='menuitem-ingredients'),
+]
          

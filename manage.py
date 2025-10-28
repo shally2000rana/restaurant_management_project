dcc2c6443510fb34b1a1f0a1a51a729648b78832
shally2000rana <itsmeshallyrana2000@gmail.com>
2025-10-28 18:29:43 +0530

    56509

--- manage.py
+++ manage.py
@@ -1,23 +1,11 @@
-from django.db import models
-from django.db.models import Count
-class MenuItemManager(models.Manager):
-    def get_top_selling_items(self, num_items=5):
-        return self.annotate(order_count=Count('orderitem'))\
-                   .order_by('order_count')[:num_items]
+from django.contrib import admin
+from .models import Restaurant
 
-class MenuItem(models.Model):
-    name=models.CharField(max_length=100)
-    price=models.DecimalField(max_digits=6, decimal_places=2)
-    objects=MenuItemManager()
-    def __str__(self):
-        return self.name
-
-class OrderItem(models.Model):
-    order=models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
-    menu_item=models.ForeignKey(MenuItem, on_delete=models.CASCADE, related_name='orderitem')
-    quantity=models.PositiveIntegerField(default=1)
-
-    def __str__(self):
-        return f"{self.menu_item.name} (x{self.quantity})"
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display=('name', 'address','phone_number','email','is_active')
+    search_fields=('name','address')
+    list_filter=('is_active',)
+    list_per_page=20
         
          

2f47024d874bbe250b8c2dfc18d0963435af793f
shally2000rana <itsmeshallyrana2000@gmail.com>
2025-10-28 17:49:12 +0530

    56506

--- manage.py
+++ manage.py
@@ -1,3 +1,19 @@
-from orders.models import Order
+import random
+from django.db import models
+
+class DailySpecial(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.TextField()
+    price=models.DecimalField(max_digits=6, decimal_places=2)
+
+    @staticmethod
+    def get_random_special():
+        specials=list(DailySpecial.objects.all())
+        if not specials:
+            return None
+        return random.choice(specials)
+    
+    def __str__(self):
+        return self.name
         
          

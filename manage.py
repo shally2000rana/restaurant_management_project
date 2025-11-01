8a1893cf5305a13fb3186619d143e110c98eece9
shally2000rana <itsmeshallyrana2000@gmail.com>
2025-11-01 19:30:25 +0530

    56966

--- manage.py
+++ manage.py
@@ -1,12 +1,16 @@
-from django.db import models
-
-class NutritionalInformation(models.Model):
-    menu_item=models.ForeignKey('MenuItem', on_delete=models.CASCADE)
-    calories=models.IntegerField()
-    protein_grams=models.DecimalField(max_digits=5, decimal_places=2)
-    fat_grams=models.DecimalField(max_digits=5, decimal_places=2)
-    carbohydrate_grams=models.DecimalField(max_digits=5, decimal_places=2)
-    def__str__(self):
-        return f"{self.menu_item.name} - {self.calories} kcal"
+from decimal import Decimal, ROUND_HALF_UP
+def calculate_tip_amount(order_total, tip_percentage):
+    """
+    calculate the tip amount for a given total and tip percentage.
+    parameters:
+      order_total(float or decimal): the total amount of the order before the tip.
+      tip_peercentage (int or float): the tip percentage
+    returns:
+      decimal : the calculated tip qmount, rounded to two decimal places.
+    """
+    order_total=Decimal(order_total)
+    tip_amount=order_total*(Decimal(tip_percentage)/Decimal(100))
+    tip_amount=tip_amount.quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
+    return tip_amount
         
          

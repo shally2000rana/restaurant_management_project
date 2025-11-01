b2136354dcd6bb867e8857744d0f075780b023fd
shally2000rana <itsmeshallyrana2000@gmail.com>
2025-11-01 19:37:42 +0530

    56967

--- manage.py
+++ manage.py
@@ -1,16 +1,17 @@
-from decimal import Decimal, ROUND_HALF_UP
-def calculate_tip_amount(order_total, tip_percentage):
+import re
+
+def validate_phone_number(phone_number):
     """
-    calculate the tip amount for a given total and tip percentage.
-    parameters:
-      order_total(float or decimal): the total amount of the order before the tip.
-      tip_peercentage (int or float): the tip percentage
-    returns:
-      decimal : the calculated tip qmount, rounded to two decimal places.
+    Validate a phone number format.
+    Accepts 10-12 digits, may include optional spaces, hypens,
+    or a country code prefix(like '+1')
+
+    Returns:
+       True if the phone number matches a valid pattern, otherwise False.
     """
-    order_total=Decimal(order_total)
-    tip_amount=order_total*(Decimal(tip_percentage)/Decimal(100))
-    tip_amount=tip_amount.quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
-    return tip_amount
+    pattern=r'^(\+?\d{1,3}[-]?)?\d{10,12}$'
+    cleaned_number=phone_number.strip()
+
+    return bool(re.match(pattern, cleaned_number))
         
          

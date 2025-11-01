72380847381219be52ffe81c6ea772b3247dc859
shally2000rana <itsmeshallyrana2000@gmail.com>
2025-11-01 19:12:26 +0530

    56964

--- manage.py
+++ manage.py
@@ -1,11 +1,12 @@
-from django.db import models
-class MenuItem(models.Model):
-    name=models.CharField(max_length=100)
-    description=models.TextField(blank=True)
-    price=models.DecimalField(max_digits=6, decimal_places=2)
-    is_featured=models.BooleanField(default=False)
+from datetime import datetime
+from your_app.models import DailyOperatingHours
 
-    def __str__(self):
-        return self.name
+def get_today_operating_hours():
+    today=datetime.today().strftime('%A')
+    try:
+        hours=DailyOperatingHours.objects.get(day=today)
+        return (hours.open_time, hours.close_time)
+    except DailyOperatingHours.DoesNotExist:
+        return (None, None)
         
          

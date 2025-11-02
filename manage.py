7a4f0600ede78219e145187bbb81c07f0f25bcb3
shally2000rana <itsmeshallyrana2000@gmail.com>
2025-11-02 19:31:58 +0530

    57981

--- manage.py
+++ manage.py
@@ -1,22 +1,12 @@
-class MenuItem(models.Model):
-    name=models.CharField(max_length=100)
-    price=models.DecimalField(max_digits=6, decimal_places=2)
-class Order(models.Model):
-    customer_name=models.CharField(max_length=100)
-    created_at=models.DateTimeField(auto_now_add=True)
-class OrderItem(models.Model):
-    order=models.ForeignKey(Order, related_name='order_items', on_delete=models.CASCADE)
-    menu_item=models.ForeignKey(MenuItem, on_delete=models.CASCADE)
-    quantity=models.PositiveIntegerField(default=1)
+from django.db import models
 
-class Order(models.Model):
-    customer_name=models.CharField(max_length=100)
-    created_at=models.DateTimeField(auto_now_add=True)
+class LoyaltyProgram(models.Model):
+    name=models.CharField(max_length=50, unique=True)
+    points_required=models.IntegerField(unique=True)
+    discount_percentage=models.DecimalField(max_digits5, decimal_places=2)
 
-    def get_unique+item_names(self):
-        items=self.order_items.all()
-        names=[item.menu_item.name for itwm in items]
-        unique_names=list(set(names))
-        return unique_names
+    def __str__(self):
+        return f"{self.name} ({self.discount_percentage}% off)"
+        
         
          

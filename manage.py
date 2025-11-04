b2297a4efed9d768d9853f96b22e16a51d2b3cff
shally2000rana <itsmeshallyrana2000@gmail.com>
2025-11-04 07:28:16 +0530

    57983

--- manage.py
+++ manage.py
@@ -1,17 +1,19 @@
-from rest_framework.test import APITestCase
-from home.models import Restaurant
+from django.db import models
+import datetime
+class UpcomingSpecialsManager(models.Manager):
+    def Upcoming(self):
+        today=datetime.date.today()
+        return super().get_queryset().filter(date_gte=today)
 
-class RestaurantInfoAPITest(APITestCase):
-    def test_get_restaurant_info(self):
-        restaurant=Restaurant.objects.create(
-            name="Test Restaurant",
-            address="123 Test St"
-        )
-        response=self.client.get('/api/restaurant-info')
+class DailySpecial(models.Model):
+    name=models.CharField(max_length=100)
+    date=models.DateField()
+    decsription=models.TextField()
 
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data['name'], restaurant.name)
-        self.assertEqual(response.data['address'], restaurant.address)
+    objects=UpcomingSpecialsManager()
+    def __str__(self):
+        return self.name
+
         
         
          

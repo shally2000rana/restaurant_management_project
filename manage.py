f186a841d4bf8a464c3878878596e27df1c8ca6c
shally2000rana <itsmeshallyrana2000@gmail.com>
2025-11-04 07:34:12 +0530

    58404

--- manage.py
+++ manage.py
@@ -1,18 +1,17 @@
-from django.db import models
-import datetime
-class UpcomingSpecialsManager(models.Manager):
-    def Upcoming(self):
-        today=datetime.date.today()
-        return super().get_queryset().filter(date_gte=today)
+from datetime import datetime
 
-class DailySpecial(models.Model):
-    name=models.CharField(max_length=100)
-    date=models.DateField()
-    decsription=models.TextField()
+def format_datetime(dt):
+    """
+    Returns a formatted string for a given datetime object.
+    Example: 'January 1, 2023 at 10:30 AM'
+    If dt is None, returns an empty string.
+    """
+    if dt is None:
+        return ""
+    if not isinstance(dt, datetime):
+        return ""
 
-    objects=UpcomingSpecialsManager()
-    def __str__(self):
-        return self.name
+    return dt.strftime("%B %D, %Y at %I:%M:%p")
 
         
         

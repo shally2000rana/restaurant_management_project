b99c966715db9159ce077c9ad5694a2f8aab5eb5
shally2000rana <itsmeshallyrana2000@gmail.com>
2025-11-09 11:32:55 +0530

    58457

--- manage.py
+++ manage.py
@@ -1,19 +1,14 @@
 from django.db import models
+from django.db.models import Q
+class Reservation(models.Model):
+    start=models.DateTimeField()
+    end=models.DateTimeField()
 
-class MenuCategory(models.Model):
-    name=models.CharField(max_length=100, unique=True)
-    decsription=models.TextField(blank=True, null=True)
-
-    def __str__(self):
-        return self.name
-
-from rest_framework import serializers
-from .models import MenuCategory
-
-class MenuCategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model=MenuCategory
-        fields='__all__'
-        
-        
-         
+    @classmethod
+    def find_available(cls, start, end):
+        conflicts=cls.objects.filter(
+            Q(start_lt=end) & Q(end__gt=start)
+        )
+        if conflicts.exists():
+            return False
+        return True       

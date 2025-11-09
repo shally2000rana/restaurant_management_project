a5d4e83be9cde2d8b88f224e1270ccadf725c055
shally2000rana <itsmeshallyrana2000@gmail.com>
2025-11-09 11:54:39 +0530

    58459

--- manage.py
+++ manage.py
@@ -1,13 +1,27 @@
-class Restaurant(models.Model):
-    name=models.CharField(max_length=150)
-    address=models.TextField()
-    phone=models.CharField(max_length=20)
-    opening_hours=models.CharField(max_length=150) 
+class MenuItem(models.Model):
+    name=models.CharField(max_length=120)
+    price=models.DecimalField(max_digits=8, decimal_places=2)
+    is_available=models.BooleanField(default=True)
 
-from rest_framework import serializers
-from .models import serializers
+from rest_framework import srializers
+from .models import MenuItem
 
-class RestaurantSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Restaurant
-        fields="__all__"
+class MenuItemAvailabilitySerializer(srializers.Serializer):
+    is_available=Serializers.BooleanField()
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import status
+from .models import MenuItem
+from .Serializers import MenuItemAvailabilitySerializer
+
+class UpdateMenuAvailabilityAPIView(APIView):
+    def patch(self, request, pk):
+        try:
+            item=MenuItem.objects.get(pk=pk)
+        except MenuItem.DoesNotExist:
+            return Response({"error": "Menu item not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        Serializer=MenuItemAvailabilitySerializer(data=request.data)
+        Serializer.is_valid(raise_exception=True)
